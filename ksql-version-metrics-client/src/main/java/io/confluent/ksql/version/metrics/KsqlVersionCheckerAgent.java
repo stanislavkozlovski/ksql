ca4c0decff6035b4ff1d915be0655dba84dbe687
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2018-08-07 11:50:46 +0100

    a

--- ksql-version-metrics-client/src/main/java/io/confluent/ksql/version/metrics/KsqlVersionCheckerAgent.java
+++ ksql-version-metrics-client/src/main/java/io/confluent/ksql/version/metrics/KsqlVersionCheckerAgent.java
@@ -23,7 +23,7 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class KsqlVersionCheckerAgent implements VersionCheckerAgent{
+public class KsqlVersionCheckerAgent implements VersionCheckerAgent {
 
   private KsqlVersionChecker ksqlVersionChecker;
 
